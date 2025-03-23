458b0502b83e6b82e6fcea7b9b0dc412971c1ab4
Toan Nguyen <hgiasac@gmail.com>
2025-03-23 22:33:28 +0700

    update asana tests

--- recipes/asana/generator/api/generate.sh
+++ recipes/asana/generator/api/generate.sh
@@ -7,15 +7,6 @@ if [ "$(uname)" == "Darwin" ]; then
   SED_CMD="sed -i .bak"
 fi
 
-curl -L https://raw.githubusercontent.com/Asana/openapi/7d1d65782705d07a0214ac5a2651b15449c680d9/defs/asana_oas.yaml -o $OAS_FILE
-
-for code in 200 201 204 400 401 402 403 404 412 424 500 501 502 503 504;
-do
-    $SED_CMD 's/'$code':/"'$code'":/g' $OAS_FILE
-done
-
-rm -f asana_oas.yaml.bak
-
 ndc-http-schema convert \
     -c config.yaml \
     -o $OUTPUT_FILE
