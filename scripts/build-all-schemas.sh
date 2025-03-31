d9f3d10aa6ba357726782f89e011a356113ec142
Toan Nguyen <hgiasac@gmail.com>
2025-03-31 17:27:16 +0700

    Upgrade ndc-http v0.10.0 (#13)

--- scripts/build-all-schemas.sh
+++ scripts/build-all-schemas.sh
@@ -1,17 +1,18 @@
 #!/bin/bash
 
-cd `dirname $0` > /dev/null
+set -e
+
+cd $(dirname $0) >/dev/null
 cd ../
 
-ROOT=`pwd -P`
+ROOT=$(pwd -P)
 RECIPES_DIR="$ROOT/recipes"
 
-for dir in $RECIPES_DIR/*
-do
+for dir in $RECIPES_DIR/*; do
   echo "$dir"
   if [ -f "$dir/Makefile" ]; then
     pushd $dir
     make build-schema
-    popd 
+    popd
   fi
 done
